1dc9e320c334e5ee2f7251f6701f4bad04225b66
sisu-01 <tesv010615@naver.com>
2024-05-09 02:43:09 +0900

    레전드 seo ㄷㄷㄷㄷ

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./about.module.css";
 
+export const metadata = {
+  title: 'About',
+  description: 'about page description',
+}
+
 const AboutPage = () => {
     return (
       <div className={styles.container}>
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./contact.module.css";
 
+export const metadata = {
+  title: 'Contact',
+  description: 'contact page description',
+}
+
 const ContactPage = () => {
     return (
       <div className={styles.container}>
